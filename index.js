67ad41de328c85de5e84de61109bed448938210f
Brittany Malooly <bmalooly@gmail.com>
2019-01-08 19:05:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 function takeANumber(katzDeli, name) {
-  katzDeli.push(name)
-  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
+  katzDeli.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }
   else {
   return (`Currently serving ${katzDeliLine.shift()}.`);
   }
 }
 function currentLine(katzDeliLine) {
-  var currentOrder = []
-  var i = 0
+  var currentOrder = [];
+  var i = 0;
   if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
   else {
     while (katzDeliLine > 0)
     return (`The line is currently: ${katzDeliLine[i]}. ${katzDeliLine.shift()}`);
-    ii+ }
+    ii+}
 }
 }
