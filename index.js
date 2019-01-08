dcf34a51b0104914b02bceed6cdcbac8eadf11ca
Brittany Malooly <bmalooly@gmail.com>
2019-01-08 19:17:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function nowServing(katzDeliLine) {
   }
 }
 function currentLine() {
-    var i = 0;
- if (katzDeli.length === 0) {
+  var i = 0;
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
   else {
