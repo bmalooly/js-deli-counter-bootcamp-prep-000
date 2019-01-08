25a5daacd30e65a6395fe6e07d58257ab5d193c3
Brittany Malooly <bmalooly@gmail.com>
2019-01-08 19:13:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty.";
   }
   else {
-    while (katzDeliLine.length > 0)
-    return (`The line is currently: ${katzDeliLine[i]}. ${katzDeliLine.shift()}`);
+    for (let i = 0; i < katzDeliLine.length; i++)
+    return (`The line is currently: ${katzDeliLine[i+1]}. ${katzDeliLine.shift()}`);
     ii+
-  }
-}
-}
